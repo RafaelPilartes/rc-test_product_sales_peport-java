06765bfa22eac92a1eb8f7866f30fc486c96fa4f
Rafael Pilartes <167252457+CyberWolfKingg@users.noreply.github.com>
2025-11-10 12:07:30 +0200

    first commit

---
+++ testPaperProductSalesQ2/src/main/java/com/mycompany/testpaperproductsalesq2/TestPaperProductSalesQ2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testpaperproductsalesq2;
+
+/**
+ *
+ * @author HP
+ */
+public class TestPaperProductSalesQ2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
